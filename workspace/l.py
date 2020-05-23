05459c79057e10aa2caf9fcbaead483dbd236050
CandyTt20 <458566293@qq.com>
2020-05-23 16:30:37 +0800

    print_demo

--- workspace/l.py
+++ workspace/l.py
@@ -1,1 +1,13 @@
-print('sds')
+class node(object):
+    def __init__(self, val, next=None):
+
+        self.val = val
+        self.next = next
+
+
+x = node(5)
+print(x.val)
+x = node(6)
+print(x.val)
+for i in range(0, 10):
+    print(i)

3da7577ebcb36cc97f462c8cbdcd25f9fc774d4b
CandyTt20 <458566293@qq.com>
2020-05-25 11:03:31 +0800

    NoneCr

--- workspace/Btree_travel.py
+++ workspace/Btree_travel.py
@@ -18,14 +18,40 @@ class Btree(object):
                 li.append(cur.rchild)
 
     def preorder(self, root):
-
+        # 递归
         if root is None:
             return
         print(root.x, end=' ')
         self.preorder(root.lchild)
         self.preorder(root.rchild)
 
+    def preorder_nocr(self):
+        # 非递归
+        stack = []
+        if self is None:
+            return
+        cur = self
+
+        while True:
+            while cur.lchild:
+                if cur.rchild:
+                    stack.append(cur)
+                print(cur.x, end=' ')
+                
+                cur = cur.lchild
+            print(cur.x, end=' ')
+            
+            if stack:
+                cur = stack.pop().rchild
+            else:
+                break
+
 
 x = Btree(5, Btree(3, Btree(1), Btree(0)), Btree(2, Btree(6)))
+y = Btree(5, Btree(3, Btree(1, Btree(8), Btree(9)), Btree(
+    0, Btree(10))), Btree(2, Btree(6), Btree(7)))
+
 # x.travel()
-x.preorder(x)
+y.preorder_nocr()
+print('')
+y.preorder(y)
